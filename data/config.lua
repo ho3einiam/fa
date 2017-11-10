684e610cce74db9deef3173a22d35c5c06dcf477
ho3einiam <33555980+ho3einiam@users.noreply.github.com>
2017-11-10 22:29:09 +0330

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,52 @@
+do local _ = {
+  about_text = "",
+  enabled_plugins = {
+    "admin",
+    "anti_spam",
+	"chat_random",
+	"cp",
+    "arabic_lock",
+    "banhammer",
+    "block",
+    "broadcast",
+    "buy",
+    "cleanall",
+    "expire",
+    "filter",
+    "help",
+    "inpm",
+    "inrealm",
+    "lock_bots",
+    "msg_checks",
+    "invite",
+    "whitelist",
+    "plugins",
+    "rmsg",
+    "setbye",
+    "setwlc",
+    "supergroups",
+    "time",
+    "saveplug",
+    "sendplug",
+    "on-off",
+    "idme",
+    "dlpl",
+    "clean_delete_ac",
+    "chat",
+    "note"
+  },
+  help_text = "",
+  help_text_realm = "",
+  help_text_super = "",
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    234724442,
+    301278951,
+	300131107,
+    0
+  }
+}
+return _
+end
